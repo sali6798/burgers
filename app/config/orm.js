b4c81fc12f4564db71b6976e28e6b4446a1bf02b
sali6798 <shaidee@alingcastre.net>
2020-04-16 12:31:47 -0700

    db queries

--- app/config/orm.js
+++ app/config/orm.js
@@ -1,21 +1,6 @@
 // Import MySQL connection.
 const connection = require("./connection");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-  const arr = [];
-
-  for (let i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   const arr = [];
@@ -54,11 +39,8 @@ const orm = {
     let queryString = "INSERT INTO " + table;
 
     queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+    queryString += cols;
+    queryString += ") VALUES (?)";
 
     console.log(queryString);
 
