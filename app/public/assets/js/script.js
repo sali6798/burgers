6e7dca1fc71cb43ff2293f889ca4edfebc05d38d
sali6798 <shaidee@alingcastre.net>
2020-04-16 18:42:19 -0700

    event listener on delete button

--- app/public/assets/js/script.js
+++ app/public/assets/js/script.js
@@ -2,20 +2,30 @@ $(function() {
     $(".create-form").submit(function(event) {
         event.preventDefault();
 
-        var newBurger = {
-            name: $("#burgerarea").val().trim()
-        };
-
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(() => {
-            console.log("new burger created");
-            location.reload();
-        });
+        const input = $("#burgerarea").val().trim();
+        // checks user doesn't add empty string
+        if (input !== "") {
+            const newBurger = {
+                name: input
+            };
+    
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(() => {
+                console.log("new burger created");
+                location.reload();
+            });
+        }
+        else {
+            // shows user that it was invalid
+            $("#burgerarea").attr("placeholder", "Must enter a burger");
+            $("#burgerarea").css("border", "2px red solid");
+        }
     });
 
     $(".change-devoured").click(function() {
+        // get id of burger
         const id = $(this).data("id");
         const isDevouredState = {
             isDevoured: true
@@ -28,5 +38,16 @@ $(function() {
             console.log("changed isDevoured to true");
             location.reload();
         })
-    })
+    });
+
+    $(".delete").click(function() {
+        const id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(() => {
+            console.log("burger deleted");
+            location.reload();
+        })
+    });
 });
